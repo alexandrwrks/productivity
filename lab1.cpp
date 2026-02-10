5995a12805dae3da39ccf7035a86c07ae930f279
alexandrwrks <alexandrwrks@gmail.com>
2026-02-10 18:17:30 +0700

    Сделана первая лаба

--- lab1.cpp
+++ lab1.cpp
@@ -3,17 +3,32 @@
 
 using namespace std;
 
-int main() {
+// int main() {
 
-    int a;
-    string number = "515-1";
-    string name = "Alexeyev Alexandr";
+//     int a;
+//     string number = "515-1";
+//     // string name = "Alexeyev Alexandr";
+//     string name = "Алексеев Александр";
     
-    cout << "Enter your age: ";
-    cin >> a;
+//     // cout << "Enter your age: ";
+//     cout << "Введите ваш возраст: ";
+//     cin >> a;
 
-    cout << name << ' ' << number << endl;
-    cout << "Your age: " << a << endl;
+//     cout << name << ' ' << "\nГрупа - " << number << endl;
+//     // cout << "Your age: " << a << endl;
+//     cout << "Ваш возраст: " << a << endl; 
     
+//     return 0;
+// }
+
+int main(){
+    // нахождение периметра прямоугольника
+    double a, b;
+
+    cout << "Введите длину и ширину: " << endl;
+    cin >> a >> b;
+
+    cout << "Периметр равен: " << (a+b)*2;
     return 0;
 }
+
