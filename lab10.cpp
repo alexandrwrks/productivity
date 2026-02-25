9b97eced0025d8e077251f24f2e5ac8bde9bbc1b
alexandrwrks <alexandrwrks@gmail.com>
2026-02-25 21:35:27 +0700

    Сделал полностью 10 лабораторку. Осталась 9 лабораторка и 9 отчётов. Проблема с файлами нужны понять эту тему и сделать её

--- lab10.cpp
+++ lab10.cpp
@@ -1,28 +1,66 @@
 #include <iostream>
 #include <vector>
+#include <fstream>
+#include <string>
 
 using namespace std;
 
-int recurseSumFunc(vector<int>& vec, size_t index = 0) {
-    if (index >= vec.size()) {
+// int recurseSumFunc(vector<int>& vec, size_t index = 0) {
+//     if (index >= vec.size()) {
+//         return 0;
+//     }
+//     return (1.0/vec[index]) + recurseSumFunc(vec, index + 1);
+// }
+
+// int main() {
+    
+//     int lenght; 
+//     cout << "Enter number of lenght: "; cin >> lenght;
+//     vector<int> massive(lenght);
+
+//     for (int i = 0; i < lenght; i++) {
+//         cout << "Enter " << i+1 << " number: ";
+//         cin >> massive[i];
+//     }
+
+//     double result = recurseSumFunc(massive);
+//     cout << "Sum of numbers: " << result;
+
+//     return 0;
+// }
+
+ifstream in;
+int sum = 0;
+
+int recurseSumNumbers() {
+    int number;
+
+    if (in >> number) {
+        return number + recurseSumNumbers();
+    } else {
         return 0;
     }
-    return (1.0/vec[index]) + recurseSumFunc(vec, index + 1);
 }
 
-int main() {
-    
-    int lenght; 
-    cout << "Enter number of lenght: "; cin >> lenght;
-    vector<int> massive(lenght);
+int main()
+{
+    string numbers;
+    cout << "Enter numbers separted by spaces: " << endl;
+    getline(cin, numbers);
 
-    for (int i = 0; i < lenght; i++) {
-        cout << "Enter " << i+1 << " number: ";
-        cin >> massive[i];
+    ofstream out("lab-10.txt");
+    if (out.is_open()) {
+        out << numbers;
+        out.close();
     }
 
-    double result = recurseSumFunc(massive);
-    cout << "Sum of numbers: " << result;
+    in.open("lab-10.txt");
+
+    if (in.is_open()) {
+        int result = recurseSumNumbers();
+        cout << "Sum = " << result << endl;
+        in.close();
+    }
 
     return 0;
 }
