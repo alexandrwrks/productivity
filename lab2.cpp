13b95dc7c7e343bd4fc1359d9365bef2c6e1500c
alexandrwrks <alexandrwrks@gmail.com>
2026-03-01 14:56:47 +0700

    Изменения для отчёта

--- lab2.cpp
+++ lab2.cpp
@@ -4,31 +4,28 @@
 
 using namespace std;
 
-int main() {
-    // Нахождение плотности жлементов
-    // p = m/V
-    float m1, m2, v1, v2;
-    cout << "Введите массу первого элемента: "; cin >> m1;
-    cout << "Введите объём первого элемента: "; cin >> v1;
-    cout << "Введите массу второго элемента: "; cin >> m2;
-    cout << "Введите объём второго элемента: "; cin >> v2;
+// int main() {
+//     // p = m/V
+//     float m1, m2, v1, v2;
+//     cout << "Enter weight 1: "; cin >> m1;
+//     cout << "Enter volume 1: "; cin >> v1;
+//     cout << "Enter weight 1: "; cin >> m2;
+//     cout << "Enter volume 1: "; cin >> v2;
 
-    float p1 = m1/v1;
-    float p2 = m2/v2;
+//     float p1 = m1/v1;
+//     float p2 = m2/v2;
 
-    if (p1 > p2) {
-        cout << "Плотность первого элемента больше:  " << p1;
-    } else {
-        cout << "Плотность второго элемента больше: " << p2;
-    }
-
-    
-    return 0;
-}
+//     if (p1 > p2) {
+//         cout << "Density 1 is greater:  " << p1;
+//     } else {
+//         cout << "Density 2 is greater: " << p2;
+//     }
+//     return 0;
+// }
 
 int main() {
     double a,b,c;
-    cout << "Введите значения для a, b, c: " << endl;
+    cout << "Enter number a, b, c: " << endl;
     cin >> a >> b >> c;
     if (a>=b & b >=c) {
         cout << "a: " << 2*a << endl;
