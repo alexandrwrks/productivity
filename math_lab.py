442a4ae0ad5cc1a67fdf2d205a7153475ddd2b28
alexandrwrks <alexandrwrks@gmail.com>
2026-03-12 17:28:14 +0700

    Создание интерефейса с графами с помощью ИИ

--- math_lab.py
+++ math_lab.py
@@ -1,69 +1,132 @@
 import networkx as nx
 import matplotlib.pyplot as plt
-# from random import randint
-
 import numpy as np
 import random
 
-const_num = 5
+from PyQt6.QtCore import QSize, Qt
+from PyQt6.QtWidgets import *
+import sys
 
-# G = nx.Graph()
-
-# G.add_nodes_from([1, 2, 3, 4, 5])
-
-# G.add_edges_from([(1, 2), (2, 3), (3, 4), (4, 5)])
-
-# plt.figure(figsize=(8, 6))
-# nx.draw(G, node_color='lightblue',  node_size=500, font_size=16, font_weight='bold')
-# plt.title("Простой неоринтерончайый граф") 
-# plt.show()
-n = int(input('Размер матрицы: '))
-
-A = [[0]*n for i in range(n)]
-for i in range(n):
-    for j in range(i+1, n):
-        A[i][j] = random.randint(0, 1)
-        A[j][i] = A[i][j]
+class MainWindow(QMainWindow):
     
-for r in A:
-    print(r)
+    def __init__(self):
+        super().__init__()
 
-A = np.array(A)
-G = nx.from_numpy_array(A)
-nx.draw(G, with_labels=True, font_size=10, linewidths=1, node_color='g')
-M = nx.incidence_matrix(G, oriented=True)
-print(M.todense())
-plt.show()
-
-
-# add_numbers_to_graph(massive)
-# class Creategraph():
-#     def __init__(self, num):
-#         self.num = num
-#         # self.create_matrix()
-#         self.create_graph()
-
-#     def create_matrix(self):
-#         print("a1 | a2 | a3 | a4 | a5")
-#         for i in range(self.num):
-#             print(f"a{i+1} |", end=" ")
-#             for i in range(self.num):
-                
-#                 num = random.randint(0, 3)
-#                 print(num, "|", end="  ")
-#             print()
+        self.button_is_checked = True
         
-#     def create_graph(self):
+        self.setWindowTitle("My app")
 
-#         G = nx.Graph()
+        button = QPushButton("Push Me")
+        button.setCheckable(True)
+        # button.clicked.connect(self.the_button_was_clicked)
+        button.clicked.connect(self.the_button_was_toggled)
+        button.setChecked(self.button_is_checked)
 
-#         G.add_edge(1, 2, weight=4.7)
-#         G.add_edges_from([(3, 4), (4, 5)], color="red")
-#         G.add_edges_from([(1, 2, {'color': 'blue'}), (2, 3, {'weight':8})])
-#         G[1][2]['weight'] = 4.7
-#         G.edges[1, 2]['weight'] = 4
+        self.setFixedSize(QSize(400, 300))
+
+        self.setCentralWidget(button)
+
+    # def the_button_was_clicked(self):
+    #     print("Clicked!")
+
+    def the_button_was_toggled(self, checked):
+        self.button_is_checked = checked
+
+        print(self.button_is_checked)
 
 
+app = QApplication(sys.argv)
+
+window = MainWindow()
+window.show()
+
+app.exec()
+
+# Ввод размера матрицы с клавиатуры
+# n = int(input('Размер матрицы: '))
+
+# def create(n):
+#     matrix = [[0] * n for _ in range(n)]
+#     for i in range(n - 1):
+#         matrix[i][i + 1] = 1
+#         matrix[i + 1][i] = 1
+#     return matrix
+
+# m = create(n)
+
+# for row in m:
+#     print(row)
+# G = nx.Graph()
+# for i in range(n):
+#     for j in range(i + 1, n):
+#         if m[i][j] == 1:
+#             G.add_edge(i, j)
+
+# # Рисуем граф
+# plt.figure(figsize=(12, 4))
+
+# # Располагаем вершины
+# pos = {i: (i * 2, 0) for i in range(n)}
+
+# # Рисуем рёбра (сделаем их потолще)
+# # nx.draw_networkx_edges(G, pos, edge_color='#555555', width=3)
+
+# # # Рисуем вершины (побольше и красивее)
+# # nx.draw_networkx_nodes(G, pos, node_color='#87CEEB',  # небесно-голубой
+# #                        node_size=1000, edgecolors='darkblue', linewidths=2)
+
+# # # Рисуем подписи (крупным шрифтом)
+# # labels = {i: i+1 for i in range(n)}
+# # nx.draw_networkx_labels(G, pos, labels, font_size=16, font_weight='bold',
+# #                         font_color='black')
 
 
-# # cg.create_matrix(5)
+# # plt.axis('off')
+# # plt.tight_layout()
+# # plt.show()
+
+# # # Создаём случайную матрицу смежности
+# # A = [[0] * n for i in range(n)]
+# # for i in range(n):
+# #     for j in range(i + 1, n):
+# #         A[i][j] = random.randint(0, 1)
+# #         A[j][i] = A[i][j]
+
+# # Выводим матрицу смежности
+# # print("\nМатрица смежности:")
+# # for r in A:
+# #     print(r)
+
+# # Преобразуем в numpy array и создаём граф
+# # A_np = np.array(A)
+# # G = nx.from_numpy_array(A_np)
+
+# # Рисуем граф
+# plt.figure(figsize=(8, 6))
+# nx.draw(G, with_labels=True, font_size=12, node_color='lightgreen',
+#         node_size=600, font_weight='bold', edge_color='gray')
+# plt.title(f"Граф с {n} вершинами")
+# plt.show()
+
+# def cmtb(adj_matrix):
+#     n = len(adj_matrix)
+#     edges = []
+
+#     for i in range(n):
+#         for j in range(i + 1, n):
+#             if adj_matrix[i][j] == 1:
+#                 edges.append((i, j))
+
+#     n_edges = len(edges)
+#     inc_matrix = [[0] * n_edges for _ in range(n)]
+
+#     for e, (v1, v2) in enumerate(edges):
+#         inc_matrix[v1][e] = 1
+#         inc_matrix[v2][e] = 1
+
+#     return inc_matrix
+
+# t = cmtb(m)
+# print('новая')
+# for row in t:
+#     print(row)
